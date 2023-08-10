72fc00e53f2318d4071c674e297a2be06d72e705
ritikad25 <120733012+ritikad25@users.noreply.github.com>
2023-08-10 19:38:59 +0530

    Add files via upload

---
+++ Diamond.java
@@ -0,0 +1,21 @@
+package patterns;
+
+public class Diamond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(i+j==4 || (i+j==6 && i>1 && i<5) || i+j==8)
+				{
+					System.out.print("*");
+				}
+				else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
